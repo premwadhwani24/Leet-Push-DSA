6916be8a927b243e3f089972031fa15d5b2e4fab
Prem wadhwani <premwadhwani24@gmail.com>
2026-08-14 22:04:31 +0530

    [189] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- 189-rotate-array/189-Rotate-Array.java
+++ 189-rotate-array/189-Rotate-Array.java
@@ -1,19 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       int n=nums.length;
-       k=k % n;
-       rotate(nums,0,n-1);
-       rotate(nums,0,k-1);
-       rotate(nums,k,n-1);
+        int n = nums.length;
+        k = k % n;
+
+        rotate(nums, 0, n - 1);
+        rotate(nums, 0, k - 1);
+        rotate(nums, k, n - 1);
     }
-       public static void rotate(int []nums,int Start,int end){
-       while(Start<end){
-       int temp=nums[Start];
-       nums[Start]=nums[end];
-       nums[end]=temp;
-       Start ++;
-       end--;
-       
+
+    public static void rotate(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
     }
-}
 }
